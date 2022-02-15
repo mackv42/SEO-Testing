cc7f27edc62192068eeac3a0a99b89c708dc59dc
Vincent Mackay <mackv42@gmail.com>
2022-02-15 03:25:24 -0600

    find elements by tag name function added

--- index.js
+++ index.js
@@ -2,15 +2,45 @@ var webdriver = require ('selenium-webdriver'),
 
   By = webdriver.By;
 
-function search(keyword){
+async function search(keyword){
 	var driver = new webdriver.Builder()
   .forBrowser('firefox')
   .build();
-	driver.get("https://www.google.com/search?q="+keyword);
+	await driver.get("https://www.google.com/search?q="+keyword);
 
 	return driver;
 }
 
-let driver = search("dogs");
+async function findElementsByTagName(driver){
+	let elements = await driver.findElements(By.xpath("//cite"));
+
+	let promises = 
+		elements.map( async function(elem){
+			let x = await elem.getText();
+			return x;
+		});
+
+	text = await Promise.all(promises);
+	return {"driver": driver, "elementsFound": elements, "text": text};
+}
+
+// Finds base url 
+async function findBaseURL(driver, base){
+	
+}
+
+
+search("dogs").then(driver => findBaseURL(driver, ""));
+
+
+setTimeout(()=>{}, 100000)
+
 //For parsing purpose we're going to extract all tags named cite 
 // Which is seemingly the tag used ( exclusive to ) search results
+
+
+// Schema: { url: STRING, pageNo: int, resultNo: int, err: string }
+// errors: [ "not found", "no results", "error connecting" ]
+
+
+
