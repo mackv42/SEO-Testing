285ba49a12078f2e6313626cafede0b19f2cd84b
Vincent Mackay <mackv42@gmail.com>
2022-02-15 21:52:50 -0600

    find Base url finds the index of the result on page

--- index.js
+++ index.js
@@ -21,26 +21,30 @@ async function getElementsByTagName(driver, tag){
 		});
 
 	let text = await Promise.all(promises);
-
-
-	return {"driver": driver, "elementsFound": elements.map((x, index) => {x.text = text[index]; return x;})};
+	return {"driver": driver, "elementsFound": elements.map((x, index) => {x.text = text[index]; x.index = index; return x;})};
 }
 
 // Finds base url 
-async function findBaseURL(driver, base){
-	
-}
-
-
-search("dogs").then(driver => getElementsByTagName(driver, "cite").then(result => console.log(result.elementsFound)));
-
-
-//For parsing purpose we're going to extract all tags named cite 
-// Which is seemingly the tag used ( exclusive to ) search results
-
+// For parsing purpose we're going to extract all tags named cite 
+// Which is the tag used for search results
 
 // Schema: { url: STRING, pageNo: int, resultNo: int, err: string }
 // errors: [ "not found", "no results", "error connecting" ]
 
+async function findBaseURL(driver, base){
+	let citeResults = await getElementsByTagName(driver, "cite");
+
+	let filtered = citeResults.elementsFound.filter(
+	 x => x.text != "" && x.text != undefined )
+		.map (
+			(elem, index) => {
+				elem.index = index+1;
+				return elem;
+			}
+		).filter( element => element.text.includes(base));
+
+	return filtered;
+}
 
 
+search("dogs").then(page => findBaseURL(page, "https://en.wikipedia.org")).then(result => console.log(result));
